502fbb2ea0c34f30b342f1628e5013e3c3484178
João Arthur Silva <126902720+JoaoArthur25@users.noreply.github.com>
2023-04-30 17:46:35 -0300

    Update Val's Adventure.c

--- Val's Adventure.c
+++ Val's Adventure.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <conio.h>
+
 int playerX = 0; // player's initial position
-int playerY = 0;
+int playerY = 0; 
 
 
 void Tutorial(){
 	
 	char option;
+	system("cls || clear");
 	printf("Val's adventure is an adventure/puzzle game where your goal is to take Val to the end of her adventure in this dungeon where she's looking for an ancient cursed weapon.\n\n");
 	printf("In each level the player must move to get a key to open the closed door.\n\n"
 	 "W: Up\n"
@@ -23,23 +25,30 @@ void Tutorial(){
 	 
 	 switch(option){
 	 	
-	 	case 'M': 
+	 	case 'm': 
 	 		system("cls || clear");
+    		Beep(650, 150);
+    		Menu();
+    		break;
+    	
+    	case 'M':
+    		system("cls || clear");
     		Beep(650, 150);
     		Menu();
     		break;
 		
 		default: 
-		system("cls || clear");
-		printf("\n");
-		printf("Choose a valid option...");
-		sleep(1);
-		Tutorial();
+			system("cls || clear");
+			printf("\n");
+			printf("Choose a valid option...");
+			sleep(1);
+			Tutorial();
     		
 	 }
 	 
 }
 char map[15][15] = { // map's bidimentional array
+
     {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
     {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
     {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
@@ -56,28 +65,28 @@ char map[15][15] = { // map's bidimentional array
     {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
     {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}
 };
-void Map() // draws the map on the screen
-{
+void Map(){ // draws the map on the screen
+
     system("cls"); 
 
     for (int y = 0; y < 15; y++)
     {
         for (int x = 0; x < 15; x++)
         {
-            if (x == playerX && y == playerY) // draws the player at the current position
-            {
+            if (x == playerX && y == playerY){   // draws the player at the current position
+            
                 printf("P");
             }
-            else
-            {
-                printf("%c", map[y][x]); // draws the corresponding character on the map
+            else{
+            
+                printf("%c", map[y][x]);       // draws the corresponding character on the map
             }
         }
         printf("\n");
     }
 }
 
-void Start(){
+void Start(){ //game 
 	
     char input;
 
@@ -85,34 +94,33 @@ void Start(){
 
     while (1) // endless loop
     {
-        input = getch(); // reads the keyboard key that was pressed
+        input = getch();             // reads the keyboard key that was pressed
 
-        switch (input)
-        {
+        switch (input){
+        	
         case 'w':
-            if (playerY > 0) // up
-            {
+            if (playerY > 0){        // up
+            
                 playerY--;
             }
             break;
 
         case 'a':
-            if (playerX > 0) // left
-            {
+            if (playerX > 0){        // left
+            
                 playerX--;
             }
             break;
 
         case 's':
-            if (playerY < 15 - 1) // down
-            {
+            if (playerY < 15 - 1){   // down
+            
                 playerY++;
             }
             break;
 
         case 'd':
-            if (playerX < 15 - 1) // right
-			{
+            if (playerX < 15 - 1){   // right
             	playerX++;
 			}
 			break;
