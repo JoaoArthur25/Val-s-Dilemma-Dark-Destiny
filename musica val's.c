a96d7bd8cd40172ada3539afcca86f9bcf183897
João Arthur Silva <126902720+JoaoArthur25@users.noreply.github.com>
2023-04-30 16:26:07 -0300

    Add files via upload

---
+++ musica val's.c
@@ -0,0 +1,97 @@
+void playNote(int frequency, int duration) {
+    Beep(frequency, duration);
+}
+
+void playmelody(int tempo) {
+    playNote(440, tempo);
+    playNote(415, tempo);
+    playNote(440, tempo);
+    playNote(415, tempo);
+    playNote(440, tempo);
+    playNote(415, tempo);
+    playNote(440, tempo);
+    playNote(415, tempo);
+    playNote(523, tempo);
+    playNote(494, tempo);
+    playNote(466, tempo);
+    playNote(440, tempo);
+    playNote(415, tempo);
+    playNote(440, tempo);
+    playNote(415, tempo);
+    playNote(440, tempo);
+    playNote(415, tempo);
+    playNote(440, tempo);
+    playNote(415, tempo);
+    playNote(523, tempo);
+    playNote(494, tempo);
+    playNote(466, tempo);
+    playNote(440, tempo);
+    playNote(415, tempo);
+    playNote(440, tempo);
+    playNote(415, tempo);
+    playNote(440, tempo);
+    playNote(415, tempo);
+    playNote(440, tempo);
+    playNote(415, tempo);
+    playNote(523, tempo);
+    playNote(494, tempo);
+    playNote(466, tempo);
+    playNote(440, tempo);
+    playNote(415, tempo);
+    playNote(440, tempo);
+    playNote(415, tempo);
+    playNote(440, tempo);
+    playNote(415, tempo);
+    playNote(440, tempo);
+    playNote(415, tempo);
+    playNote(440, tempo);
+    playNote(415, tempo);
+    playNote(392, tempo);
+    playNote(369, tempo);
+    playNote(311, tempo);
+    playNote(329, tempo);
+    playNote(311, tempo);
+    playNote(294, tempo);
+    playNote(311, tempo);
+    playNote(294, tempo);
+    playNote(311, tempo);
+    playNote(294, tempo);
+    playNote(311, tempo);
+    playNote(294, tempo);
+    playNote(311, tempo);
+    playNote(294, tempo);
+    playNote(311, tempo);
+    playNote(294, tempo);
+    playNote(311, tempo);
+    playNote(294, tempo);
+    playNote(311, tempo);
+    playNote(294, tempo);
+    playNote(311, tempo);
+    playNote(294, tempo);
+    playNote(311, tempo);
+    playNote(294, tempo);
+    playNote(311, tempo);
+    playNote(294, tempo);
+    playNote(311, tempo);
+    playNote(294, tempo);
+    playNote(311, tempo);
+    playNote(294, tempo);
+    playNote(311, tempo);
+    playNote(294, tempo);
+    playNote(311, tempo);
+    playNote(294, tempo);
+    playNote(311, tempo);
+    playNote(294, tempo);
+    playNote(311, tempo);
+    playNote(294, tempo);
+    playNote(311, tempo);
+    playNote(294, tempo);
+    playNote(311, tempo);
+    playNote(294, tempo);
+    playNote(311, tempo);
+    playNote(294, tempo);
+}
+
+int main(void){
+playmelody(1500);
+}
